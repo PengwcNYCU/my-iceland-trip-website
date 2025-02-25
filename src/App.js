3d0f941dad96e9763e6c83897a7f88e64d74a89d
Wei-Cheng, Peng <wesleym6m6@gmail.com>
2025-02-25 22:49:17 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import TravelItinerary from './components/TravelItinerary';
+
+function App() {
+  return (
+    <div className="App">
+      <TravelItinerary />
+    </div>
+  );
+}
+
+export default App;
